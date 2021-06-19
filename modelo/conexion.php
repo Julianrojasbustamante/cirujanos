f917baf8e2f0be8d6d3ec8a363940791324cd016
Julián Rojas Bustamante <julian.rjs15@hotmail.com>
2021-06-19 16:27:11 -0500

    test

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -3,10 +3,10 @@
 class Conexion{
 	
 	static public function conectar(){
-		$conexionDb = new PDO("mysql:host=localhost;dbname=cirujanos","root","Croydon.2021",array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+		$conexionDb = new PDO("mysql:host=localhost;dbname=cirujanos","root","",array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 		return $conexionDb;
 	}
-
+	
 	// public function conectar(){
 	// 	$conexionDb = new PDO("mysql:host=localhost;dbname=latienda_productos","latienda_product","wKtntz8WTvM38c3Fop",array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     //                       PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
